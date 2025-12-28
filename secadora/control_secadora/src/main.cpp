98b0cbe04b3ace3824cce8404d997970e0f4247a
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-12-28 14:43:39 +0100

    update

--- secadora/control_secadora/src/main.cpp
+++ secadora/control_secadora/src/main.cpp
@@ -1,54 +1,31 @@
 #include <Arduino.h>
 
-// ---- Command sequences ----
-const byte CMD_READY[] = {'r', 'e', 'a', 'd', 'y', '\n', '\r'}; // "ready\r\n"
-const byte RELAY1_ON[] = {0xA0, 0x01, 0x01, 0xA2};
-const byte RELAY1_OFF[] = {0xA0, 0x01, 0x00, 0xA1};
-const byte RELAY2_ON[] = {0xA0, 0x02, 0x01, 0xA3};
-const byte RELAY2_OFF[] = {0xA0, 0x02, 0x00, 0xA2};
+const uint8_t RELAY1_PIN = 10;
+const uint8_t RELAY2_PIN = 11;
 
-void sendCommand(const byte *cmd, size_t len) {
-  Serial.write(cmd, len);
-  Serial.flush();
+void relayOn(uint8_t relayPin) {
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);
 }
 
-void toggleLed() {
-  static bool initialized = false;
-  static uint8_t state = LOW;
-  if (!initialized) {
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, state);
-    initialized = true;
-  }
-  state = (state == LOW) ? HIGH : LOW;
-  digitalWrite(LED_BUILTIN, state);
+void relayOff(uint8_t relayPin) {
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
 }
 
-void setup() {
-  // Hardware Serial used for relay board
-  Serial.begin(115200);
-  delay(1000);
-
-  // Send handshake
-  sendCommand(CMD_READY, sizeof(CMD_READY));
-  delay(500);
-}
+void setup() {}
 
 void loop() {
-  toggleLed();
-  // Toggle relays in sequence
-  sendCommand(RELAY1_ON, sizeof(RELAY1_ON));
+
+  relayOn(RELAY1_PIN);
   delay(2000);
 
-  toggleLed();
-  sendCommand(RELAY2_ON, sizeof(RELAY2_ON));
+  relayOff(RELAY1_PIN);
   delay(2000);
 
-  toggleLed();
-  sendCommand(RELAY1_OFF, sizeof(RELAY1_OFF));
+  relayOn(RELAY2_PIN);
   delay(2000);
 
-  toggleLed();
-  sendCommand(RELAY2_OFF, sizeof(RELAY2_OFF));
+  relayOff(RELAY2_PIN);
   delay(2000);
 }
